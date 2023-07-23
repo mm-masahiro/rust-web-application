54d6356834ca72cc98f4cdd395dc13382ea66b2a
mm-masahiro <hand5.mm9.mzh@gmail.com>
2023-07-23 22:01:36 +0900

    test get request

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, App, HttpResponse, HttpServer};
+
+#[get("/")]
+async fn index() -> Result<HttpResponse, actix_web::Error> {
+    let response_body = "Hello, world!";
+
+    Ok(HttpResponse::Ok().body(response_body))
+}
+
+#[actix_web::main]
+async fn main() -> Result<(), actix_web::Error> {
+    HttpServer::new(move || App::new().service(index))
+        .bind("127.0.0.1:8080")?
+        .run()
+        .await?;
+    Ok(())
 }
